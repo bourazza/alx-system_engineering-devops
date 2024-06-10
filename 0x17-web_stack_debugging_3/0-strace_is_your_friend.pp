53346e844ce6b26e26700456db2b66d4728703c9
bourazza <zakinirvana482@gmail.com>
2024-06-10 02:21:52 +0000

    strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/usr/local/bin', '/bin'],
-=======
- bad "phpp" extensions to "php" in "wp-settings.php".
-exec{'fix-wordpress':
-command => 'sed -i s/phpp/php/g/var/www/html/wp-settings.php', path => '/usr/local/bin/:/bin/'
->>>>>>> 22faa64f78fd3b22974cb8ae22c2bc60e6dd4d41
+# puppet file to automate a 500 error fix
+exec { 'fixed-phpp':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin';
 }
