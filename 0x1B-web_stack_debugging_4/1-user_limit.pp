8e186a7656216a591d10a25c533f00d18ecf6421
bourazza <zakinirvana482@gmail.com>
2024-06-13 22:17:40 +0000

    1 zuser limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,7 @@
-# hanges the limitations on the holberton user
+# Changes the limitations on the holberton user
 exec { 'change-os-configuration-for-holberton-user':
   command => "sed -iE 's/^holberton hard nofile 5/holberton hard nofile 88888/; s/^holberton soft nofile 4/holberton soft nofile 88888/' /etc/security/limits.conf",
   path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }
+
 
